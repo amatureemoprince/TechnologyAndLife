3d3875f172955806471c3d2b3e1650e02b47be86
lj <2069057986@qq.com>
2025-03-13 22:24:41 +0800

    tree

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,13 +2,12 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 export default defineUserConfig(
     {
-      base: "/",
       lang: "zh-CN",
       title: "Java咖啡馆",
       description: "热爱技术、享受生活",
       theme,
           head: [
-                ['link', { rel: 'icon', href: '/TechnologyAndLife/favicon.ico' }], // 设置 favicon
+                ['link', { rel: 'icon', href: '/favicon.ico' }], // 设置 favicon
           ],
       // 和 PWA 一起启用
       // shouldPrefetch: false,
