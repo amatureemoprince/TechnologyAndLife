c140622e73e0529fa26248d5e1ad96412b28fb01
lj <2069057986@qq.com>
2025-03-28 15:58:52 +0800

    baidu-map-site

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,7 @@ export default defineUserConfig(
       theme,
           head: [
                 ['link', { rel: 'icon', href: '/favicon.ico' }], // 设置 favicon
+                ['meta', { name: 'baidu-site-verification', content: 'codeva-EW7fPgf7Zs' }],
           ],
       // 和 PWA 一起启用
       // shouldPrefetch: false,
